051442a5d8862baae81d0e248f27e25bf89c4395
idistaviso <Qawsedzxc123>
2024-05-02 16:42:50 +0300

    step 2

--- src/App.js
+++ src/App.js
@@ -6,45 +6,34 @@ import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
+import PostForm from "./components/PostForm";
+import postForm from "./components/PostForm";
 
 function App() {
 
-    const [posts, setPosts] = useState([
-        {id: 1, title: 'Javascript', body: 'Description'},
-        {id: 2, title: 'Javascript 2', body: 'Description'},
-        {id: 3, title: 'Javascript 3', body: 'Description'}
-    ])
-
-    const [post, setPost] = useState({title: '', body: ''});
-
-    const addNewPost = (e) => {
-        e.preventDefault()
-
-        setPosts([...posts, {...post, id: Date.now()}])
-        setPost({title: '', body: ''})
+    const [posts, setPosts] = useState([{id: 1, title: 'Javascript', body: 'Description'}, {
+        id: 2,
+        title: 'Javascript 2',
+        body: 'Description'
+    }, {id: 3, title: 'Javascript 3', body: 'Description'}])
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost])
+    }
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id))
     }
 
-    return (
-        <div className="App">
-            <form>
-                {/*Управляемый компонент*/}
-                <MyInput
-                    value={post.title}
-                    onChange={e => setPost({...post, title: e.target.value})}
-                    type="text"
-                    placeholder="Название поста"
-                />
-                {/*Неуправляемый\Неконтролируемый компонент*/}
-                <MyInput
-                    value={post.body}
-                    onChange={e => setPost({...post, body: e.target.value})}
-                    type="text"
-                    placeholder="Описание поста"/>
-                <MyButton onClick={addNewPost}>Создать пост</MyButton>
-            </form>
-            <PostList posts={posts} title="Посты про JS"/>
-        </div>
-    );
+    return (<div className="App">
+        <PostForm create={createPost}/>
+        {posts.length !== 0
+            ?
+            <PostList remove={removePost} posts={posts} title="Посты про JS"/>
+            :
+            <h1 style={{textAlign: "center"}}>
+                Посты не найдены!
+            </h1>
+        }
+    </div>);
 }
 
 export default App;
