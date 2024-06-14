70ad74c8efa455aaf45866a29f958be1d4c33294
Idistaviso <idista@yandex.ru>
2024-06-14 11:27:09 +0300

    Pagination 2:12:00

--- src/App.js
+++ src/App.js
@@ -16,7 +16,8 @@ import axios from "axios";
 import PostService from "./API/PostService";
 import Loader from "./components/UI/Loader/Loader";
 import {useFetching} from "./hooks/useFetching";
-import {getPageCount} from "./utils/pages";
+import {getPageCount, getPagesArray} from "./utils/pages";
+import Pagination from "./components/UI/pagination/Pagination";
 
 function App() {
 
@@ -28,17 +29,16 @@ function App() {
     const [page, setPage] = useState(1);
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
-    const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
-        const response = await PostService.getAll(limit, page);
-        setPosts(response.data)
-        const totalCount = response.headers['x-total-count']
-        setTotalPages(getPageCount(totalCount, limit));
-    })
-
-    console.log(totalPages)
+    const [fetchPosts, isPostsLoading, postError] = useFetching(
+        async (limit, page) => {
+            const response = await PostService.getAll(limit, page);
+            setPosts(response.data)
+            const totalCount = response.headers['x-total-count']
+            setTotalPages(getPageCount(totalCount, limit));
+        })
 
     useEffect(() => {
-        fetchPosts()
+        fetchPosts(limit, page)
     }, []);
 
     const createPost = (newPost) => {
@@ -49,12 +49,18 @@ function App() {
         setPosts(posts.filter(p => p.id !== post.id))
     }
 
+    const changePage = (page) => {
+        setPage(page)
+        fetchPosts(limit, page)
+    }
+
     return (<div className="App">
-        <button onClick={fetchPosts}>GET POSTS</button>
         <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
             Создать пользователя
         </MyButton>
-        <MyModal visible={modal} setVisible={setModal}>
+        <MyModal
+            visible={modal}
+            setVisible={setModal}>
             <PostForm create={createPost}/>
         </MyModal>
         <hr style={{margin: '15px 0'}}/>
@@ -63,11 +69,17 @@ function App() {
             setFilter={setFilter}
         />
         {postError &&
-            <h1>Произошла ошибка ${postError}</h1>}
+            <h1>Произошла ошибка ${postError}</h1>
+        }
         {isPostsLoading
             ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div>
             : <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS"/>
         }
+        <Pagination
+            page={page}
+            changePage={changePage}
+            totalPages={totalPages}/>
+
     </div>);
 }
 
