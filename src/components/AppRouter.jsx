5c6d9f57bc7dc7f986c6d053787f0c6cbf599d1d
idistaviso <Qawsedzxc123>
2024-06-17 16:35:59 +0300

    useHistory, useParams 2:29:36

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -3,13 +3,15 @@ import {Route, Routes} from "react-router-dom";
 import Posts from "../pages/Post";
 import About from "../pages/About";
 import Error from "../pages/Error";
+import PostIdPage from "../pages/PostIdPage";
 
 const AppRouter = () => {
     return (
         <Routes>
-            <Route path="/posts" element={<Posts/>}/>
-            <Route path="/about" element={<About/>}/>
-            <Route path="/*" element={<Error/>}/>
+            <Route path="posts" element={<Posts/>}/>
+            <Route path="posts/:id" element={<PostIdPage/>}/>
+            <Route path="about" element={<About/>}/>
+            <Route path="*" element={<Error/>}/>
         </Routes>
     );
 };
